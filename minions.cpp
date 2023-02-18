2d8a73529bc605fcd86f7092ed325dd5c71738a8
Pallavi Deshmukh <pallavideshmukh32897@gmail.com>
2023-02-18 15:58:55 -0500

    add modified files to activity-minions

--- minions.cpp
+++ minions.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <thread>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
 
+using namespace std;
+
+void minion();
+void overlord();
 
 int main (int argc, char** argv) {
   if (argc < 2) {
@@ -8,5 +16,28 @@ int main (int argc, char** argv) {
   }
   //Your code goes here
   
+  int cnt = std::stoi(argv[1]);
+
+  for(int i =0; i < cnt; i++)
+  {
+    std::thread t1 (minion);
+    t1.join();
+  }  
+
+  std::thread myThread2 (overlord);
+  
+  myThread2.join();
+
   return 0;
 }
+
+void minion()
+{
+	std::cout<<"Hello! I am minion ";
+}
+
+void overlord()
+{
+	std::cout<<"Hello Minions! I am the Overlord ";
+}
+
