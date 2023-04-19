410f2a0352b5585f1236ff5603fdb7a8d1dc2987
Pallavi Nanasaheb Deshmukh <pdeshmu5@gal-i1.uncc.edu>
2023-04-18 23:39:09 -0400

    Modified

--- hello_world/hello.cpp
+++ hello_world/hello.cpp
@@ -2,19 +2,23 @@
 #include <unistd.h>
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char*argv[]) 
 {
-	int rank;
-	int size;
-  	int length;
-  	char machine[MPI_MAX_PROCESSOR_NAME];
+	int a;
+	int b;
+  	int c;
+
+  	char system[MPI_MAX_PROCESSOR_NAME];
   
 	MPI_Init(NULL, NULL);
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Get_processor_name( machine, &length);
-	cout<<"I am a process: "<<rank<<" out of: "<<size<<" I am running on "<<machine<<"\n";
+	MPI_Comm_rank(MPI_COMM_WORLD, &a);
+	MPI_Comm_size(MPI_COMM_WORLD, &b);
+	MPI_Get_processor_name( system, &c);
+
+	cout<<"I am a process: "<<a<<" out of: "<<b<<" I am running on "<<system<<"\n";
 	MPI_Finalize();
 	
 	return 0;
